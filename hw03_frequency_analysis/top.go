7543d9918b2509c204971e6b6519186e58bc8c0d
vivakhnyuk <vivakhnyuk@diasoft.ru>
2024-11-18 20:28:24 +0300

    hw3 done!

--- hw03_frequency_analysis/top.go
+++ hw03_frequency_analysis/top.go
@@ -1,6 +1,42 @@
 package hw03frequencyanalysis
 
-func Top10(_ string) []string {
-	// Place your code here.
-	return nil
+import (
+	"regexp"
+	"sort"
+)
+
+type wordFrequency struct {
+	word  string
+	count int
+}
+
+var re = regexp.MustCompile(`\S+`)
+
+func Top10(text string) []string {
+	words := re.FindAllString(text, -1)
+
+	wordCount := make(map[string]int)
+
+	for _, word := range words {
+		wordCount[word]++
+	}
+
+	frequencies := make([]wordFrequency, 0, len(wordCount))
+	for word, count := range wordCount {
+		frequencies = append(frequencies, wordFrequency{word, count})
+	}
+
+	sort.Slice(frequencies, func(i, j int) bool {
+		if frequencies[i].count == frequencies[j].count {
+			return frequencies[i].word < frequencies[j].word
+		}
+		return frequencies[i].count > frequencies[j].count
+	})
+
+	topWords := []string{}
+	for i := 0; i < len(frequencies) && i < 10; i++ {
+		topWords = append(topWords, frequencies[i].word)
+	}
+
+	return topWords
 }
