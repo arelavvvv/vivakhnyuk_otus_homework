5ba36e578c49c809a72a294e25bc56ca6002ca3a
vivakhnyuk <vivakhnyuk@diasoft.ru>
2025-02-11 00:10:44 +0300

    fix copy and pb

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"path/filepath"
 
 	//nolint:depguard
 	"github.com/cheggaaa/pb/v3"
@@ -13,18 +14,40 @@ import (
 var (
 	ErrUnsupportedFile       = errors.New("unsupported file")
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+	ErrSameFile              = errors.New("source and destination are the same file")
+	ErrUnknownFileSize       = errors.New("cannot use progress bar with file of unknown size (e.g., /dev/urandom)")
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
+	absFromPath, err := filepath.Abs(fromPath)
+	if err != nil {
+		return fmt.Errorf("ошибка при получении абсолютного пути для исходного файла: %w", err)
+	}
+
+	absToPath, err := filepath.Abs(toPath)
+	if err != nil {
+		return fmt.Errorf("ошибка при получении абсолютного пути для целевого файла: %w", err)
+	}
+
+	if absFromPath == absToPath {
+		return ErrSameFile
+	}
+
 	srcFile, err := os.Open(fromPath)
 	if err != nil {
 		return fmt.Errorf("ошибка при открытии исходного файла: %w", err)
 	}
 	defer srcFile.Close()
 
-	srcInfo, err := srcFile.Stat()
+	dstFile, err := os.Create(toPath)
 	if err != nil {
-		return fmt.Errorf("ошибка при получении информации о файле: %w", err)
+		return fmt.Errorf("ошибка при создании целевого файла: %w", err)
+	}
+	defer dstFile.Close()
+
+	srcInfo, err := srcFile.Stat()
+	if err != nil { // e.g. /dev/urandom
+		return ErrUnknownFileSize
 	}
 
 	if offset > srcInfo.Size() {
@@ -32,22 +55,22 @@ func Copy(fromPath, toPath string, offset, limit int64) error {
 	}
 
 	var bytesToCopy int64
+	remainingBytes := srcInfo.Size() - offset
+
 	if limit == 0 {
-		bytesToCopy = srcInfo.Size() - offset
+		bytesToCopy = remainingBytes
 	} else {
-		bytesToCopy = limit
+		if limit > remainingBytes {
+			bytesToCopy = remainingBytes
+		} else {
+			bytesToCopy = limit
+		}
 	}
 
 	_, err = srcFile.Seek(offset, io.SeekStart)
 	if err != nil {
 		return fmt.Errorf("ошибка при перемещении указателя файла: %w", err)
 	}
-
-	dstFile, err := os.Create(toPath)
-	if err != nil {
-		return fmt.Errorf("ошибка при создании целевого файла: %w", err)
-	}
-	defer dstFile.Close()
 
 	bar := pb.Full.Start64(bytesToCopy)
 	barReader := bar.NewProxyReader(srcFile)
