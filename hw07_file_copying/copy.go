12fa12b17e1fe0e63de4b49915b293cafcab5775
vivakhnyuk <vivakhnyuk@diasoft.ru>
2025-02-10 14:58:27 +0300

    hw7 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,12 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
+
+	//nolint:depguard
+	"github.com/cheggaaa/pb/v3"
 )
 
 var (
@@ -10,6 +16,48 @@ var (
 )
 
 func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+	srcFile, err := os.Open(fromPath)
+	if err != nil {
+		return fmt.Errorf("ошибка при открытии исходного файла: %w", err)
+	}
+	defer srcFile.Close()
+
+	srcInfo, err := srcFile.Stat()
+	if err != nil {
+		return fmt.Errorf("ошибка при получении информации о файле: %w", err)
+	}
+
+	if offset > srcInfo.Size() {
+		return fmt.Errorf("offset больше, чем размер файла: %d > %d", offset, srcInfo.Size())
+	}
+
+	var bytesToCopy int64
+	if limit == 0 {
+		bytesToCopy = srcInfo.Size() - offset
+	} else {
+		bytesToCopy = limit
+	}
+
+	_, err = srcFile.Seek(offset, io.SeekStart)
+	if err != nil {
+		return fmt.Errorf("ошибка при перемещении указателя файла: %w", err)
+	}
+
+	dstFile, err := os.Create(toPath)
+	if err != nil {
+		return fmt.Errorf("ошибка при создании целевого файла: %w", err)
+	}
+	defer dstFile.Close()
+
+	bar := pb.Full.Start64(bytesToCopy)
+	barReader := bar.NewProxyReader(srcFile)
+
+	_, err = io.CopyN(dstFile, barReader, bytesToCopy)
+	if err != nil && !errors.Is(err, io.EOF) {
+		return fmt.Errorf("ошибка при копировании данных: %w", err)
+	}
+
+	bar.Finish()
+
 	return nil
 }
--- hw07_file_copying/copy_test.go
+++ hw07_file_copying/copy_test.go
@@ -1,7 +1,95 @@
 package main
 
-import "testing"
+import (
+	"io"
+	"os"
+	"testing"
+
+	//nolint:depguard
+	"github.com/stretchr/testify/require"
+)
 
 func TestCopy(t *testing.T) {
-	// Place your code here.
+	t.Run("offset 0, limit 0", func(t *testing.T) {
+		require.Equal(t, nil, Copy("testdata/input.txt", "out.txt", 0, 0))
+		require.Equal(t, true, FEqual("out.txt", "testdata/out_offset0_limit0.txt"))
+		require.Equal(t, nil, os.Remove("out.txt"))
+	})
+
+	t.Run("offset 0, limit 10", func(t *testing.T) {
+		require.Equal(t, nil, Copy("testdata/input.txt", "out.txt", 0, 10))
+		require.Equal(t, true, FEqual("out.txt", "testdata/out_offset0_limit10.txt"))
+		require.Equal(t, nil, os.Remove("out.txt"))
+	})
+
+	t.Run("offset 0, limit 1000", func(t *testing.T) {
+		require.Equal(t, nil, Copy("testdata/input.txt", "out.txt", 0, 1000))
+		require.Equal(t, true, FEqual("out.txt", "testdata/out_offset0_limit1000.txt"))
+		require.Equal(t, nil, os.Remove("out.txt"))
+	})
+
+	t.Run("offset 0, limit 10000", func(t *testing.T) {
+		require.Equal(t, nil, Copy("testdata/input.txt", "out.txt", 0, 10000))
+		require.Equal(t, true, FEqual("out.txt", "testdata/out_offset0_limit10000.txt"))
+		require.Equal(t, nil, os.Remove("out.txt"))
+	})
+
+	t.Run("offset 100, limit 1000", func(t *testing.T) {
+		require.Equal(t, nil, Copy("testdata/input.txt", "out.txt", 100, 1000))
+		require.Equal(t, true, FEqual("out.txt", "testdata/out_offset100_limit1000.txt"))
+		require.Equal(t, nil, os.Remove("out.txt"))
+	})
+
+	t.Run("offset 6000, limit 1000", func(t *testing.T) {
+		require.Equal(t, nil, Copy("testdata/input.txt", "out.txt", 6000, 1000))
+		require.Equal(t, true, FEqual("out.txt", "testdata/out_offset6000_limit1000.txt"))
+		require.Equal(t, nil, os.Remove("out.txt"))
+	})
+}
+
+func FEqual(file1Path, file2Path string) bool {
+	file1, err := os.Open(file1Path)
+	if err != nil {
+		return false
+	}
+	defer file1.Close()
+
+	file2, err := os.Open(file2Path)
+	if err != nil {
+		return false
+	}
+	defer file2.Close()
+
+	buffer1 := make([]byte, 32*1024)
+	buffer2 := make([]byte, 32*1024)
+
+	for {
+		n1, err1 := file1.Read(buffer1)
+		if err1 != nil && err1 != io.EOF {
+			return false
+		}
+
+		n2, err2 := file2.Read(buffer2)
+		if err2 != nil && err2 != io.EOF {
+			return false
+		}
+
+		if err1 == io.EOF && err2 == io.EOF {
+			return true
+		}
+
+		if err1 == io.EOF || err2 == io.EOF {
+			return false
+		}
+
+		if n1 != n2 {
+			return false
+		}
+
+		for i := 0; i < n1; i++ {
+			if buffer1[i] != buffer2[i] {
+				return false
+			}
+		}
+	}
 }
